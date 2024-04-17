201af80917e18399b48bb3b44b93ea1b8aaa7cd5
Akulohcuek0 <ibrahimkholilulafham@gmail.com>
2024-04-17 13:48:09 +0700

    backup

---
+++ regizai/app/Http/Controllers/DashboardController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+class DashboardController extends Controller
+{
+    //
+
+    public function index()
+    {
+
+        $name = Auth::User()->name;
+
+        if(!Auth::check())
+        {
+            return redirect('/');
+        } else {
+            $user = Auth::user();
+            if ($user->email != NULL) {
+                return view('page.dashboard', ['name' => $name]);
+            } else {
+                if ($user->gender == "male" || $user->gender == "female") {
+                    return view('page.dashboard', ['name' => $name]);
+                } else if ($user->gender == null) {
+                    return redirect('choosegender');
+                }
+            }
+        }
+    }
+}
---
+++ regizai/resources/views/auth/chooseGender.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Choose Gender</title>
+
+    <link rel="stylesheet" href="{{ asset('css/styleGender.css') }}">
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
+    <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+    <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+
+</head>
+<body>
+
+    <div class="content-perent">
+         <div class="content-text">
+            <h1 class="judul">CHOOSE GENDER</h1>
+            <p class="judul-dalam">Just only two</p>
+        </div>
+        <form action="{{ url('/biodata')}}" method="POST">
+            @csrf
+            <div class="content-gender">  
+                <input name="email" value="{{ $email }}" hidden>
+                <input name="password" value="{{ $password }}" hidden>
+                <div class="icon1 mr-5">
+                    <img src="{{URL::asset('/img/gcowo.jpg')}}" alt="profile Pic" height="200" width="200">
+                    <p class="description">Male</p>
+
+                    <div class="radio">
+                        <input type="radio" id="html" name="gender" value="male" required>
+                    </div>
+
+                </div>
+                <div class="ml-5">
+                    <img src="{{URL::asset('/img/gcewe.jpg')}}" alt="profile Pic" height="200" width="200">
+                    <p class="description">Female</p>
+
+                    <div class="radio">
+                        <input type="radio" id="css" name="gender" value="female" required>
+                    </div>
+
+                </div>
+            </div>
+            <div class="buton">
+                <button class="btn btn-primary tombol" type="submit">Next</button>
+            </div>   
+        </form>
+    </div>
+
+</body>
+</html>
