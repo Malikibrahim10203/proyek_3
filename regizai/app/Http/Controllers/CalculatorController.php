298fa1378721bee69c077f179cf0aea8da919651
Akulohcuek0 <ibrahimkholilulafham@gmail.com>
2024-06-13 15:46:23 +0700

    edit lagi bro

--- regizai/app/Http/Controllers/CalculatorController.php
+++ regizai/app/Http/Controllers/CalculatorController.php
@@ -7,13 +7,13 @@ use Illuminate\Http\Request;
 class CalculatorController extends Controller
 {
     //
-    public function index()  
+    public function index()
     {
-        
+
         if(isset($_POST['calculate'])) {
             $width  = $_POST['width'];
             $height = $_POST['height'];
-            
+
             $BMI = $height / ($width * $width);
 
             return view('page/calculator',["BMI"=>$BMI]);
